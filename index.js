ed9fad9e1f5bd63f3a1a834711de69ee8002f595
erickmwei680@gmail.com <erickmwei680@gmail.com>
2024-03-28 13:39:46 +0300

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
 const app = express();
 
+// Make an api request using express server
+app.get("/", (req,res)=>{
+    res.json("Hello this is the backend")
+})
 
 app.listen(5000, ()=>{
     console.log("API working!")
